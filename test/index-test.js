01b9f13996540252dbb45319fd4157008ed437b6
Pasqualino de Simone <pasalino@gmail.com>
2018-04-30 15:42:52 +0200

    add empty folder

--- test/index-test.js
+++ test/index-test.js
@@ -3,6 +3,7 @@
 // const { Server } = require('mock-socket');
 // const { expect } = require('chai');
 
+require('../app/app');
 
 describe('WebSocketMiddleware', () => {
   it('Opens a socket and connect to it', (done) => {
